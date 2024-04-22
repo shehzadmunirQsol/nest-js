9e8447b9b7c4ac3daf45dbc75588a48a9c368e85
unknown <shehzadmunir.qsols@gmail.com>
2024-04-22 11:34:35 +0500

    added logging functionality

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -7,24 +7,34 @@ import {
   Param,
   Delete,
   Query,
+  Ip,
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
-
+import { SkipThrottle, Throttle } from '@nestjs/throttler';
+import { MyLoggerService } from 'src/my-logger/my-logger.service';
+@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
-
+  private readonly logger = new MyLoggerService(EmployeesController.name);
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeesService.create(createEmployeeDto);
   }
-
+  @SkipThrottle({ default: false })
   @Get()
-  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+  findAll(
+    @Ip() ip: string,
+    @Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN',
+  ) {
+    this.logger.log(
+      `Request for All Employees \t ${ip}`,
+      EmployeesController.name,
+    );
     return this.employeesService.findAll(role);
   }
-
+  @Throttle({ short: { ttl: 60000, limit: 1 } })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
---
+++ src/my-logger/my-logger.module.ts
@@ -0,0 +1,8 @@
+import { ConsoleLogger, Module } from '@nestjs/common';
+import { MyLoggerService } from './my-logger.service';
+
+@Module({
+  providers: [MyLoggerService],
+  exports: [MyLoggerService],
+})
+export class MyLoggerModule {}
